fbf055ef9747db043a282da43997fc96790fe1ba
Quit975 <plushquotient@gmail.com>
2018-11-14 23:12:50 +0100

    GUI prototype framework

---
+++ qPlayer/gui.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "core.h"
+
+#include <SFML/Graphics.hpp>
+
+class GUI
+{
+private:
+	sf::RenderWindow m_window;
+
+public:
+	GUI();
+	void run();
+
+
+
+
+};
---
+++ qPlayer/main.cpp
@@ -0,0 +1,12 @@
+#include "gui.h"
+
+
+int main() {
+	GUI gui;
+	gui.run();
+	
+
+
+
+	return 0;
+}
