39c6b4e98fc78db8e69576e8e1f1d5982c062565
Quit975 <plushquotient@gmail.com>
2018-11-14 23:10:59 +0100

    core.h

---
+++ qPlayer/core.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/*
+Core header defining debug values, debug utils, errors, exceptions, etc.
+qPlayer
+*/
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#ifndef NDEBUG
+#define DEBUG(x) do {std::cout << x << std::endl;} while (false)
+const bool debugMode = true;
+#else
+#define DEBUG(x)
+const bool debugMode = false;
+#endif // NDEBUG
+
+#define LOG(x) do {std::cout << x << std::endl;} while (false)
+#define FAIL(x) do {std::cerr << "Op failed: " << x << std::endl;} while (false)
+#define ERR(x) do {std::cerr << "[" << __FILE__ << "][" << __FUNCTION__ << "][Line " << __LINE__ << "] " << x << std::endl;} while (false)
